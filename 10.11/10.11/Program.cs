7d0ce43641dcf9bbbd9374885fa64d125f3645b6
Raul Iacob <137762504+iacobraul@users.noreply.github.com>
2025-11-10 13:54:04 +0200

    .

---
+++ 10.11/10.11/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10._11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {   //cate nr din secventa au aceeasi valoare cu pozitia lor
+            int contor = cautaInSecventa();
+
+            if (contor == 0)
+                Console.WriteLine("Nu exista numere in secventa egale cu pozitia pe care se afla");
+            else
+                Console.WriteLine($"S-au gasit {contor} numere egale cu pozitia pe care se afla");
+
+        }
+
+        static int cautaInSecventa()
+        {
+            int i = 0;
+            int contor = 0;
+
+            string input = Console.ReadLine();
+
+            foreach (string numarString in input.Split(' '))
+            {
+                if(int.TryParse(numarString, out int numarintreg))
+                {
+                    if(numarintreg == i)
+                        contor++;
+                    i++; 
+                }
+                else
+                {
+                    Console.WriteLine($" {numarintreg} nu este nr intreg");
+                }
+            }
+
+            return contor;
+        }
+    }
+}
---
+++ SecventaMinMaxValue/SecventaMinMaxValue/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecventaMinMaxValue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var rezultat = MinMaxInSecventa();
+
+            int? minVal = rezultat.minVal;
+            int? maxVal = rezultat.maxVal;
+
+            if (minVal.HasValue && maxVal.HasValue)
+            {
+                Console.WriteLine($"Valoarea minima din secventa este {minVal.Value}");
+                Console.WriteLine($"Valoarea maxima din secventa este: {maxVal.Value}");
+            }
+            else
+            {
+                Console.WriteLine("Secventa nu contine elemente valide.");
+            }
+        }
+
+        static (int? minVal,int? maxVal) MinMaxInSecventa()
+        {
+            string input = Console.ReadLine();
+
+            int? minVal = null;
+            int? maxVal = null;
+
+            foreach(string numarString in input.Split(' '))
+            {
+                if (int.TryParse(numarString, out int numarintreg))
+                {
+                    if (!minVal.HasValue || numarintreg < minVal.Value)
+                    {
+                        minVal = numarintreg;
+                    }
+                    if (!maxVal.HasValue || numarintreg > maxVal.Value)
+                    {
+                        maxVal = numarintreg;
+                    }
+                }
+                else
+                    Console.WriteLine($"{numarintreg} nu este un numar");
+            }
+            return (minVal, maxVal);
+        }
+    }
+}
